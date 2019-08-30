ca48bb1f537bbf930ce294dd1b70794bdf207664
Anusha Prabhakar <51149041+anushaPrabhakar08@users.noreply.github.com>
2019-08-30 11:44:47 +0530

    Add files via upload

---
+++ src/components/imageConstant.js
@@ -0,0 +1,42 @@
+import home from "../assets/images/Dummy Icon.svg";
+import dashboard from "../assets/images/dashboard.svg";
+import effortmanagement from "../assets/images/effort management.svg";
+import feedback from "../assets/images/feedback.svg";
+import Info from "../assets/images/Info.svg";
+import dashboardSelected from "../assets/images/dashboard-selected.svg";
+import requests from "../assets/images/requests.svg";
+import reports from "../assets/images/reports.svg";
+import usermanagement from "../assets/images/user management.svg";
+import configuration from "../assets/images/configuration.svg";
+import repository from "../assets/images/repository.svg";
+import upload from "../assets/images/upload.svg";
+import search from "../assets/images/search.svg";
+
+import downarrow from "../assets/images/Group_1802.svg";
+/* dropdown Menu Icons */
+import tick from "../assets/images/tick.svg";
+import drowdownarrow from '../assets/images/drowdown arrow.svg';
+import dummyicon from '../assets/images/Dummy Icon.svg';
+import Cascadingicon from '../assets/images/Cascading menu arrow icon.svg'
+
+export {
+  home,
+  dashboard,
+  dashboardSelected,
+  effortmanagement,
+  feedback,
+  Info,
+  requests,
+  usermanagement,
+  configuration,
+  reports,
+  repository,
+  upload,
+  search,
+  downarrow,
+  /* dropdown menu icons*/
+  tick,
+  drowdownarrow,
+  dummyicon,
+  Cascadingicon
+};
